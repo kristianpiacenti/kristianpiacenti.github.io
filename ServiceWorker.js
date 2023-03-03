1346d47a60e8bd6efd68220a49724264f625d39d
kristianpiacenti <piacentikristian@hotmail.it>
2023-03-03 11:55:46 +0100

    Test build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Schindler Configurator-0.1.0";
+const contentToCache = [
+    "Build/Schindler Configurator Builds.loader.js",
+    "Build/Schindler Configurator Builds.framework.js.br",
+    "Build/Schindler Configurator Builds.data.br",
+    "Build/Schindler Configurator Builds.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
